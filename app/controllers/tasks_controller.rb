cdddcaa14c47d5316ebe2a2efa78b0407dda234c
Ivan Talijancic <ivan.talijancic@teladochealth.com>
2023-02-23 15:33:17 -0300

    Add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,29 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  # View action notes:
+  #   - Create an instance variable of new entry on db
+  #   - Render a view with a form to create a new entry
   def new
     @count = Task.count
     @task = Task.new(position: @count + 1)
   end
 
+  # Create action notes:
+  #   - Instantiate a new object using form parameters
+  #   - Save the object
+  #   - If save succeeds, redirect to the index action
+  #   - If save fails, redisplay the for so user can fix problems
   def create
-
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      # The new action is nOT being called here
+      # Assign any instance variables needed
+      # @count = Task.count
+      render('new')
+    end
   end
   def edit
   end
@@ -29,5 +45,11 @@ class TasksController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def task_params
+    params.required(:task).permit(:name, :position, :completed, :description)
   end
 end
